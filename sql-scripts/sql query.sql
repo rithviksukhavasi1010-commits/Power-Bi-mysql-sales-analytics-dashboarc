a1681593f0a22a7c9efdb80ed4dd656b7d7798ca
rithviksukhavasi1010-commits <rithvik.sukhavasi1010@gmail.com>
2026-05-20 05:41:25 +0530

    Add files via upload

---
+++ sql-scripts/sql query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM prod.`prod+env+inventory+dataset`;
+
+update prod.`prod+env+inventory+dataset` 
+set 'product id' = 7 where 'product id'=21
+
+update prod.`prod+env+inventory+dataset`
+set 'product id' =11 where 'product id'=22
